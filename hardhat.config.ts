4e93e6bb33a6f35cf18c741c19970fb71024d6cd
LongShao007 <007longshao@gmail.com>
2021-05-08 16:46:25 +0800

    deploy token contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,7 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(await account.address);
   }
 });
 
@@ -19,5 +19,23 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
  */
 module.exports = {
   solidity: "0.7.3",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.alchemyapi.io/v2/TPd3LuYkl3c0c1tX2zBRgiwmqkENWOTA"
+        // url: "https://mainnet.infura.io/v3/5ff7611cd7534bc1b7c196d3441f5841"
+      }
+    },
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/60f1cb26c41244a8bb7770d9638e04d5",
+      accounts: ["863c1532484a20dd7d68fb3dac3eb422b4daf7ad4acd244f1eb8c52748c81404"]
+    },
+    ropsten: {
+      // url: "https://ropsten.infura.io/v3/5ff7611cd7534bc1b7c196d3441f5841",
+      url: "https://eth-ropsten.alchemyapi.io/v2/tQfxutB8jYV7IxY4czqE0gYZ-o8lTrVx",
+      accounts: ["863c1532484a20dd7d68fb3dac3eb422b4daf7ad4acd244f1eb8c52748c81404"]
+    }
+  }
 };
 
